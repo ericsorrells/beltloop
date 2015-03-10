7dfe370c7d44623237f59fcb453fb6f423804883
Eric Sorrells <ericsorrells@hotmail.com>
2015-03-09 22:13:43 -0400

    added sessions info and finished auth process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,37 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-  format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }
+  before_create :create_remember_token 
+
+  #validations
+  validates :name,  presence: true, length: { maximum: 50 } 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 254 },
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :phone, presence: true, length: { is: 10 }
   has_secure_password
+
+  def self.find_user_by_phone_num(number)
+    number.to_i
+    User.where('phone = ?', number)
+  end
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def answered_questions
+    self.answers.where.not(content: nil).questions
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root "users#new"
   resources :users
   match '/signup',  to: 'users#new', via: 'get'
